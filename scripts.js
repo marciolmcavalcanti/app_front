55f9f59ac1881a0dd60188db8aaf35007bf85636
Marcio Motta <marciolmc70@gmail.com>
2023-09-21 19:38:28 -0300

    Versão Final MVP

--- scripts.js
+++ scripts.js
@@ -1,47 +1,58 @@
 /*
+  -------------------------------------------------------------------------------------
+  Limpa a tabela de bolões preparando para uma nova carga de dados
+  -------------------------------------------------------------------------------------
+*/
+const limparTabela = () => {
+  var table = document.getElementById('myTable');
+  while (table.rows.length > 1) {
+    table.deleteRow(1);
+  }
+}
+
+
+/*
   --------------------------------------------------------------------------------------
-  Função para obter a lista existente do servidor via requisição GET
+  Função para obter a lista de bolões existente do servidor - Método GET
   --------------------------------------------------------------------------------------
 */
 const getList = async () => {
-  let url = 'http://127.0.0.1:5000/produtos';
+  let url = 'http://127.0.0.1:5000/boloes';
+
   fetch(url, {
     method: 'get',
   })
     .then((response) => response.json())
     .then((data) => {
-      data.produtos.forEach(item => insertList(item.nome, item.quantidade, item.valor))
+      limparTabela();
+      data.boloes.forEach(item => insertList(item.id, item.nome, item.cotas, item.valor))
     })
     .catch((error) => {
       console.error('Error:', error);
     });
 }
 
-/*
-  --------------------------------------------------------------------------------------
-  Chamada da função para carregamento inicial dos dados
-  --------------------------------------------------------------------------------------
-*/
-getList()
-
 
 /*
   --------------------------------------------------------------------------------------
-  Função para colocar um item na lista do servidor via requisição POST
+  Função para adicionar um bolão no banco de dados - Método POST
   --------------------------------------------------------------------------------------
 */
 const postItem = async (inputProduct, inputQuantity, inputPrice) => {
   const formData = new FormData();
   formData.append('nome', inputProduct);
-  formData.append('quantidade', inputQuantity);
+  formData.append('cotas', inputQuantity);
   formData.append('valor', inputPrice);
 
-  let url = 'http://127.0.0.1:5000/produto';
+  let url = 'http://127.0.0.1:5000/add_bolao';
   fetch(url, {
     method: 'post',
     body: formData
   })
-    .then((response) => response.json())
+    .then((response) => {
+      response.json();
+      getList();
+    })
     .catch((error) => {
       console.error('Error:', error);
     });
@@ -50,14 +61,15 @@ const postItem = async (inputProduct, inputQuantity, inputPrice) => {
 
 /*
   --------------------------------------------------------------------------------------
-  Função para criar um botão close para cada item da lista
+  Função para criar um botão de excluir para cada item da lista
   --------------------------------------------------------------------------------------
 */
 const insertButton = (parent) => {
   let span = document.createElement("span");
-  let txt = document.createTextNode("\u00D7");
+  let imgExcluir = document.createElement("img");
+  imgExcluir.src = "img/lixeira.png";
   span.className = "close";
-  span.appendChild(txt);
+  span.appendChild(imgExcluir);
   parent.appendChild(span);
 }
 
@@ -69,42 +81,43 @@ const insertButton = (parent) => {
 */
 const removeElement = () => {
   let close = document.getElementsByClassName("close");
-  // var table = document.getElementById('myTable');
   let i;
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function () {
       let div = this.parentElement.parentElement;
-      const nomeItem = div.getElementsByTagName('td')[0].innerHTML
+      const idItem = div.getElementsByTagName('td')[0].innerHTML
       if (confirm("Você tem certeza?")) {
         div.remove()
-        deleteItem(nomeItem)
+        deleteItem(idItem)
         alert("Removido!")
       }
     }
   }
 }
 
+
 /*
   --------------------------------------------------------------------------------------
-  Função para deletar um item da lista do servidor via requisição DELETE
+  Função para deletar um Boão no banco de dados - Método DELETE
   --------------------------------------------------------------------------------------
 */
-const deleteItem = (item) => {
-  console.log(item)
-  let url = 'http://127.0.0.1:5000/produto?nome=' + item;
+const deleteItem = (idItem) => {
+  console.log(idItem)
+  let url = 'http://127.0.0.1:5000/bolao?id=' + idItem;
   fetch(url, {
     method: 'delete'
-  })
+    })
     .then((response) => response.json())
     .catch((error) => {
       console.error('Error:', error);
     });
 }
 
+
 /*
-  --------------------------------------------------------------------------------------
-  Função para adicionar um novo item com nome, quantidade e valor 
-  --------------------------------------------------------------------------------------
+  -----------------------------------------------------------------------------------------
+  Função para adicionar um novo Bolão no banco de dados e atualizar a Tabela da Página
+  -----------------------------------------------------------------------------------------
 */
 const newItem = () => {
   let inputProduct = document.getElementById("newInput").value;
@@ -112,34 +125,86 @@ const newItem = () => {
   let inputPrice = document.getElementById("newPrice").value;
 
   if (inputProduct === '') {
-    alert("Escreva o nome de um item!");
-  } else if (isNaN(inputQuantity) || isNaN(inputPrice)) {
-    alert("Quantidade e valor precisam ser números!");
+    alert("Informe o nome do Bolão!");
+  } else if (isNaN(inputQuantity) || (inputQuantity === "") || isNaN(inputPrice) || (inputPrice === "")) {
+    alert("Cotas e valor precisam ser números!");
   } else {
-    insertList(inputProduct, inputQuantity, inputPrice)
-    postItem(inputProduct, inputQuantity, inputPrice)
-    alert("Item adicionado!")
+
+    // Adiciona no banco de dados
+    postItem(inputProduct, inputQuantity, inputPrice);    
+    
+    alert("Bolão adicionado!");
+  }
+}
+
+
+/*
+  -----------------------------------------------------------------------------------------
+  Função para buscar bolões de uma determinada loteria - Método GET
+  -----------------------------------------------------------------------------------------
+*/
+const findItens = () => {
+  let nomeBolao = document.getElementById("newInput").value;
+  let url = 'http://127.0.0.1:5000/bolao?nome=' + nomeBolao;
+
+  if (nomeBolao === '') {
+    //alert("Informe o nome do Bolão!");
+    getList();
+  } else {
+    fetch(url, {
+      method: 'get'
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        limparTabela();
+        data.boloes.forEach(item => insertList(item.id, item.nome, item.cotas, item.valor))
+      })
+      .catch((error) => {
+        alert("Nenhum bolão encontrado para a Loteria selecionada!");
+      });
   }
 }
 
 /*
   --------------------------------------------------------------------------------------
-  Função para inserir items na lista apresentada
+  Função para inserir Bolões na Tabela da Página
   --------------------------------------------------------------------------------------
 */
-const insertList = (nameProduct, quantity, price) => {
-  var item = [nameProduct, quantity, price]
+const insertList = (idBolao, nameProduct, quantity, price) => {
+  var item = [idBolao, nameProduct, quantity, price]
   var table = document.getElementById('myTable');
   var row = table.insertRow();
+  let valor = 0;
 
+  // Adiciona o novo bolão na tabela
   for (var i = 0; i < item.length; i++) {
     var cel = row.insertCell(i);
-    cel.textContent = item[i];
+
+    if (i == 3) {
+      valor = item[i];
+      cel.textContent = valor.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+    } else {
+      cel.textContent = item[i];
+    }
+    
+    ultimoId = item[0];
   }
+
+  // Cria o botão de excluir o item da tabela
   insertButton(row.insertCell(-1))
+
+  // Limpa os campos de inserção dos dados
   document.getElementById("newInput").value = "";
   document.getElementById("newQuantity").value = "";
   document.getElementById("newPrice").value = "";
 
   removeElement()
 }
+
+
+/*
+  --------------------------------------------------------------------------------------
+  Chamada da função para carregamento inicial dos dados
+  --------------------------------------------------------------------------------------
+*/
+getList();
